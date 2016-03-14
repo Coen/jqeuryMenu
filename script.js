9b2d2bbb238bab114b2e99f367ad0568dc0ed495
Coen Filipsen <coen-psv@hotmail.com>
2016-03-14 21:19:44 +0100

    Script af

--- script.js
+++ script.js
@@ -1,8 +1,21 @@
-var scrollWaarde;
+var scrollValue;
+var LastPosistion = 0;
+var nav = $('nav');
+var menuHeight = nav.outerHeight();
 
 function scrollMenu(){
-scrollWaarde = $(this).scrollTop();
-  console.log(scrollWaarde);
-}
+  scrollValue = $(this).scrollTop();
+    if (scrollValue>LastPosistion) {
+      //We scrollen naar beneden.
+      console.log('we scrollen naar beneden');
+      LastPosistion=scrollValue;
+      nav.css('top', -menuHeight,'px');
+    } else {
+      console.log('haal het menu');
+      LastPosistion = scrollValue;
+      nav.css('top', 0);
+
+    }
+  }
 
 $(window).on('scroll', scrollMenu);
